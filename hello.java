48fa028f0cb1ad1aaa6a8e27d873783fe443c2f0
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-07-08 11:43:32 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Hello{
+  public static void main(String args[])throws IOException 
+  {
+    int i;
+    
+    for(i=0;i<5;i++)  {
+      
+    System.out.println("Hello");
+    }
+    }
+    }
+    

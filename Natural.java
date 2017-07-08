03f9ec259bd94ab1213fb06ec6748058156254ca
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-07-08 16:12:45 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Natural{
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number");
+    int a,i=1,sum=0;
+    a=sc.nextInt();
+    while(i<=a)
+    {
+      sum = sum+i;
+      i++;
+      }
+      System.out.println("the natural number is " +sum);
+      }
+      }
+      
+    
+  

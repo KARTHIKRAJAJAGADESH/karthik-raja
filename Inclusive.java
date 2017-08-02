5a0d42e849d58328bf0339437765e304d55d223f
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-08-02 11:22:35 +0530

    Create Inclusive.java

---
+++ Inclusive.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;   
+  public class Inclusive {
+      public static void main (String[] args) {
+          Scanner input = new Scanner(System.in);
+          int total=0;
+          int numbers=15;
+          int count;
+          int counter=0;
+          
+          {
+          while (counter < numbers)
+          {
+              total = total + numbers;
+              counter++;
+          }
+          count = total;
+          }
+          int i=15,limit=45,sum=0;
+               
+                for(i=15; i <= limit; i++){
+                       
+                        if( i % 2 != 0){
+                                    
+                                //System.out.println(i + " ");
+                                 sum = sum + i;
+                                }
+                                
+                                }
+          System.out.println("the sum of count 15 is "+ count);
+          System.out.println("the sum of odd count is "+ sum);
+                                                                }
+                                                                }
+  

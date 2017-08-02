27407ad630ae4a9c9490e59d1f0cb4c932dbc32b
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-08-02 11:41:25 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class Largest{
+  public static void main(String args[])throws IOException 
+  {
+    int a,b,c;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    
+    if(a>=b && a>=c) 
+{
+System.out.println("Largest number: "+a);
+}
+if(b>=a && b>=c) 
+{
+System.out.println("Largest number: "+b);
+}
+if(c>=a && c>=b) 
+{
+System.out.println("Largest number: "+c);
+}
+}
+}
+
+    

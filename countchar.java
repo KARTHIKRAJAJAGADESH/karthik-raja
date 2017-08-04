4f51e73da97cd0c848b4f4f615d2175622222c83
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-08-04 09:54:33 +0530

    Create countchar.java

---
+++ countchar.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Countchar {
+  public static void main(String args[]) throws IOException {
+ 
+    String a=" ";
+    int len;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the name");
+    a=br.readLine();
+    len=a.length();
+    System.out.println(len);
+    
+         }
+         }
+  
